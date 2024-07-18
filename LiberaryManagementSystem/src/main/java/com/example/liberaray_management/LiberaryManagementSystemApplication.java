636029c03bd36e44e0575f9da5e9ecac85643404
sindhu7989 <sindhu6673@gmail.com>
2024-07-18 10:52:17 +0530

    first commit

---
+++ LiberaryManagementSystem/src/main/java/com/example/liberaray_management/LiberaryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.liberaray_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberaryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberaryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ LiberaryManagementSystem/src/test/java/com/example/liberaray_management/LiberaryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liberaray_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberaryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
